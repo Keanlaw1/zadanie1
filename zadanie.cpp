23537a244b0c50f77cdc12c321fff89a956e73d6
Grigoriy Kovynev <kovynevgrisha@gmail.com>
2024-04-15 23:23:51 +0400

    рекурсивная функиця fib(n)

--- zadanie.cpp
+++ zadanie.cpp
@@ -1,7 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 
+int fib(int n)
+{
+    if ((n == 1) || (n == 2))
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
     cout << "Hello World!\n";
+    cout << fib(10);
+    return 0;
 }
